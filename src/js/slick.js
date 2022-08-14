85c5a8f16c2e7dad036a2547496aa19bf66f17ed
Anastasia-Filipenko <filipenko.nastyha@gmail.com>
2022-08-14 14:36:07 +0300

    final version of the section

--- src/js/slick.js
+++ src/js/slick.js
@@ -1,1 +1,12 @@
-$('.single-item').slick({dots:true, arrows:false}) ; 
+$('.single-item').slick({
+  dots: true,
+  arrows: false,
+  speed: 1450,
+  easing: 'linear',
+  autoplay: true,
+  autoplaySpeed: 1500,
+  infinite: true,
+  pauseOnFocus:true,
+  pauseOnHover:true,
+  pauseOnDotsHover:true
+});
