40aa26864fa4c282d449253d3de6c0ed0dd4473d
FinikBranson <105942136+FinikBranson@users.noreply.github.com>
2022-08-14 14:01:10 +0000

    Deploying to gh-pages from @ FinikBranson/simpsons-icecream@223d579f48c65c47e679425645a0dcfede74a705 🚀

--- index.11bd980a.js
+++
@@ -1,2 +0,0 @@
-$(".single-item").slick({dots:!0,arrows:!1,speed:1450,easing:"linear",autoplay:!0,autoplaySpeed:1500,infinite:!0,pauseOnFocus:!0,pauseOnHover:!0,pauseOnDotsHover:!0});
-//# sourceMappingURL=index.11bd980a.js.map
