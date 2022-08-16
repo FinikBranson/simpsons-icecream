acaa4fc196cd40f7530decd9222f5a70d0bc860d
Anastasia-Filipenko <filipenko.nastyha@gmail.com>
2022-08-17 00:26:31 +0300

    added scroll

--- src/js/scrol.js
+++ src/js/scrol.js
@@ -1,4 +1,3 @@
-
 import $ from 'jquery';
 
 $(document).ready(function () {
@@ -18,7 +17,7 @@ $(document).ready(function () {
         {
           scrollTop: $(hash).offset().top,
         },
-        800,
+        1010,
         function () {
           // Add hash (#) to URL when done scrolling (default click behavior)
           window.location.hash = hash;
