82a02ff8f3c393782eea4d3c98e6c947c1cd58a1
Karina2500 <karina.bogdan2000@gmail.com>
2022-08-16 21:40:13 +0200

    modals

--- src/js/modal-contacs.js
+++ src/js/modal-contacs.js
@@ -14,3 +14,20 @@
     refs.body.classList.toggle('no-scroll');
   }
 })();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-contacs-loc-open]'),
+    closeModalBtn: document.querySelector('[data-modal-contacs-loc-close]'),
+    modal: document.querySelector('[data-modal-contacs-loc]'),
+    body: document.querySelector('body'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
+  }
+})();
